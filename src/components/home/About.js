41ad0e1c742f6e30e0b7935f487ed4cde600afb4
Henry Doan <henrydoan96@gmail.com>
2022-08-15 10:29:45 -0600

    update about

--- src/components/home/About.js
+++ src/components/home/About.js
@@ -18,10 +18,10 @@ const About = () => (
           Sillicon Slopes
         </DateSec>
         <AboutPara>
-          Hello, I am Henry and I love to build really cool stuff and to spread my knowledge to others. 
+          Hello, I am Henry and I have a passion to build really awesome products and to spread my knowledge to others. 
         </AboutPara> 
         <AboutPara>
-          I didn't start off in the industry but quickly learn to love it. My interest grew when I started in 2014 on learning Web Development and saw how I can produce magic. With simple lines of code, it can produce a extraordinary results with the more I code, the more I improve the user experiences and continue to code to this day. With the exciting new advances in techonolgies, I can never get bored of what I do and am alway learning to adapt to the new changes. 
+          I didn't start off in the industry but quickly learn to love it. My interests grew when I started in 2014 on learning Web Development and saw how I can produce magic. With simple lines of code, it can produce a extraordinary results, with the more I code, the more I improve the user experiences and continue to code to this day. With the exciting new advances in techonolgies, I can never get bored of what I do and am alway learning to adapt to the new changes. 
         </AboutPara>
         <AboutPara>
           Fast forward to what I do now, of developing <AboutLink to='/projects'>Applications</AboutLink>, and spread my knowledge on platforms such as my <AboutOutSourceLink href="https://hd-coding.netlify.app/" target="_blank" rel="noopener noreferrer">Youtube</AboutOutSourceLink>, personal <AboutOutSourceLink href="https://hd-blog-henry-doan.vercel.app/" target="_blank" rel="noopener noreferrer">Blog</AboutOutSourceLink> and instruct a Part Time Full Stack Web Development course at <AboutOutSourceLink href="https://www.devpointlabs.com/" target="_blank" rel="noopener noreferrer">DevPoint Labs</AboutOutSourceLink>. 
